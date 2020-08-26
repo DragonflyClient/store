b9032a46ed29b0ec6f0f8d085e691f417e7025de
flaaaps <julianwening4@gmail.com>
2020-08-27 00:02:33 +0200

    🗑 Create database connection

---
+++ mongoose.js
@@ -0,0 +1,9 @@
+require('dotenv/config')
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@45.85.219.34:27017/dragonfly`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    console.log('Connected to DB'))
+
+const connection = mongoose.connection
+
+module.exports = connection
