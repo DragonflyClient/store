b36369dd83e21e7e26a135a54aa11108f0b51590
flaaaps <julianwening4@gmail.com>
2020-08-28 02:06:20 +0200

    🚀 Add some middleware

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const checkoutRoute = require('./routes/checkout')
 const connection = require('./mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const ejs = require('ejs');
 
 const app = express();
 
+// EJS middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+app.use(cors())
+
+// Body parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => res.render('index'));
 
+bodyParser.raw({ type: 'application/json' })
 app.use('/checkout', checkoutRoute)
 
 
