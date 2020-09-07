87e3e0b746d215d6d1ee0c4b192ff3af1b5a0e51
flaaaps <julianwening4@gmail.com>
2020-09-07 21:49:28 +0200

    🗃 Change find to finOne

--- routes/referral.js
+++ routes/referral.js
@@ -5,18 +5,17 @@ const mongoose = require('mongoose');
 
 router.get('/:partner', async (req, res) => {
     const partner = req.params.partner
-    const results = await mongoose.connection.db.collection('ref-links').find({ name: partner.toString().toLowerCase() });
-    let refs = [];
-    await results.forEach((result) => refs.push(result));
-    console.log(partner, refs)
+    const refLink = await mongoose.connection.db.collection('ref-links').findOne({ name: partner.toString().toLowerCase() });
+    // let refs = [];
 
-    if (refs && refs.length > 0) {
+    if (refLink !== null) {
+
         const itemsCollection = await mongoose.connection.db.collection('shop-items').find({});
 
         let items = [];
         await itemsCollection.forEach((result) => items.push(result));
         res
-            .cookie('ref', refs[0].name, { expires: new Date(Date.now() + 3600000), sameSite: "Lax" })
+            .cookie('ref', refLink.name, { expires: new Date(Date.now() + 3600000), sameSite: "Lax" })
             .status(201)
             .render('index', { shopItems: items, ref: partner })
     } else {
