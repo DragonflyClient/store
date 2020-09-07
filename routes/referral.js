3dfd4f6718c1aec1502763ca0220479afdd30317
flaaaps <julianwening4@gmail.com>
2020-09-07 20:22:15 +0200

    ✨ Add route to check ref link

---
+++ routes/referral.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { connection, findItemById } = require('../mongoose.js');
+const mongoose = require('mongoose');
+
+router.get('/:partner', async (req, res) => {
+    const partner = req.params.partner
+    const results = await mongoose.connection.db.collection('referral-links').find({ partnerName: partner.toString().toLowerCase() });
+    let refs = [];
+    await results.forEach((result) => refs.push(result));
+    console.log(partner, refs)
+
+    if (refs && refs.length > 0) {
+        const itemsCollection = await mongoose.connection.db.collection('shop-items').find({});
+
+        let items = [];
+        await itemsCollection.forEach((result) => items.push(result));
+        res
+            .cookie('ref', refs[0].partnerName, { expires: new Date(Date.now() + 3600000), sameSite: "Lax" })
+            .status(201)
+            .render('index', { shopItems: items, ref: partner })
+    } else {
+        res.status(404)
+            .clearCookie('ref', { domain: "store.playdragonfly.net" })
+            .redirect('/')
+    }
+})
+
+module.exports = router
