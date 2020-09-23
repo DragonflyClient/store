1e5db97a96b84444d980e0f2e204c59dd618c9ab
flaaaps <julianwening4@gmail.com>
2020-09-23 21:47:10 +0200

    🚀 Create info route for referrals

--- routes/referral.js
+++ routes/referral.js
@@ -3,6 +3,44 @@ const router = express.Router();
 const { connection, findItemById } = require('../mongoose.js');
 const mongoose = require('mongoose');
 const Referral = require('../models/Referral')
+const axios = require('axios').default
+
+router.get('/info', async (req, res) => {
+    const dragonflyToken = req.cookies["dragonfly-token"]
+    if (!dragonflyToken) return res.send({ success: false, message: 'A dragonfly token is required' })
+    const result = await axios.post('https://api.playdragonfly.net/v1/authentication/token', {}, {
+        headers: {
+            "Authorization": `Bearer ${dragonflyToken}`
+        }
+    })
+
+    const collectionRefLinks = mongoose.connection.db.collection('ref-links')
+    const dragonflyUUID = result.data.uuid
+
+    const refLink = await collectionRefLinks.findOne({ uuid: dragonflyUUID })
+
+    if (!refLink) return res.status(400).send({ success: false, message: `Ref link with uuid ${dragonflyUUID} not found` })
+    console.log(result.data, refLink)
+
+    res.status(200).send({ success: true, ref: refLink })
+})
+
+router.post('/type/:name/:type', async (req, res) => {
+    const { name, type } = req.params
+    const validRefTypes = ["discount", "bonus"]
+    console.log(validRefTypes.includes(type))
+    if (!validRefTypes.includes(type)) return res.status(400).send({ message: "Invalid ref type" })
+
+    const collectionRefLinks = mongoose.connection.db.collection('ref-links')
+    const refLink = await collectionRefLinks.findOne({ name: name })
+    if (refLink.type === type) return res.status(200).send({ message: `Ref type already set to ${type}` })
+
+    const updatedRefLink = await collectionRefLinks.findOneAndUpdate({ name: name }, { $set: { type: type } })
+
+    if (!refLink) return res.status(400).send({ message: `Referral with name ${name} could not found` })
+    console.log(updatedRefLink)
+    res.send({ name: name, type: type, ref: updatedRefLink })
+})
 
 router.get('/:partner', async (req, res) => {
     const partner = req.params.partner
@@ -25,23 +63,6 @@ router.get('/:partner', async (req, res) => {
             .clearCookie('ref', { domain: "store.playdragonfly.net" })
             .redirect('/')
     }
-})
-
-router.post('/type/:name/:type', async (req, res) => {
-    const { name, type } = req.params
-    const validRefTypes = ["discount", "bonus"]
-    console.log(validRefTypes.includes(type))
-    if (!validRefTypes.includes(type)) return res.status(400).send({ message: "Invalid ref type" })
-
-    const collectionRefLinks = mongoose.connection.db.collection('ref-links')
-    const refLink = await collectionRefLinks.findOne({ name: name })
-    if (refLink.type === type) return res.status(200).send({ message: `Ref type already set to ${type}` })
-
-    const updatedRefLink = await collectionRefLinks.findOneAndUpdate({ name: name }, { $set: { type: type } })
-
-    if (!refLink) return res.status(400).send({ message: `Referral with name ${name} could not found` })
-    console.log(updatedRefLink)
-    res.send({ name: name, type: type, ref: updatedRefLink })
 })
 
 module.exports = router
