7d2bb5338d8d3bcb33593920aa7f1b1a8a26ee32
flaaaps <julianwening4@gmail.com>
2020-09-24 19:38:44 +0200

    🏗 Re-structure routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const axios = require('axios')
+
+const MODE = process.env.MODE
 
 const ejs = require('ejs');
 
@@ -22,6 +25,34 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+bodyParser.raw({ type: 'application/json' });
+app.use('/checkout', checkoutRoute);
+
+app.use('/ref', referralRoute)
+
+
+// Security while development
+app.get('/*', async (req, res) => {
+    console.log(MODE)
+    if (MODE === "DEVELOPMENT") {
+        const dragonflyToken = req.cookies["dragonfly-token"]
+        if (!dragonflyToken) return res.render("error", { message: 'Please login in order to see this content.', backUrl: "https://playdragonfly.net", paymentId: null })
+
+        const result = await axios.post('https://api.playdragonfly.net/v1/authentication/token', {}, {
+            headers: {
+                "Authorization": `Bearer ${dragonflyToken}`
+            }
+        })
+        if (result.data.permissionLevel <= 8) return res.render("error", { message: 'You don\'t have permission to access this resource.', backUrl: "https://playdragonfly.net", paymentId: null })
+    }
+    const shopItems = await mongoose.connection.db.collection('shop-items').find({});
+
+    let items = [];
+    await shopItems.forEach((item) => items.push(item));
+    res.clearCookie('ref')
+        .render('index', { shopItems: items, refName: null, refType: null })
+})
+
 app.get('/', async (req, res) => {
     const results = await mongoose.connection.db.collection('shop-items').find({});
 
@@ -31,11 +62,5 @@ app.get('/', async (req, res) => {
         .render('index', { shopItems: items, refName: null, refType: null })
 
 });
-
-bodyParser.raw({ type: 'application/json' });
-app.use('/checkout', checkoutRoute);
-
-app.use('/ref', referralRoute)
-
 app.listen(process.env.PORT, () => console.log(`Server Started on ${process.env.URL}`));
 
