614907c6b1f271a04a2e9c533140414c4a727ee5
flaaaps <julianwening4@gmail.com>
2020-09-25 22:02:52 +0200

    🚀 Use body instead of route parameter

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const axios = require('axios').default
+const bodyParser = require('body-parser');
 
-
+router.use(bodyParser.urlencoded())
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 router.get('/ref/info', async (req, res) => {
     const dragonflyToken = req.cookies["dragonfly-token"]
     if (!dragonflyToken) return res.send({ success: false, message: 'A dragonfly token is required' })
@@ -12,7 +16,7 @@ router.get('/ref/info', async (req, res) => {
             "Authorization": `Bearer ${dragonflyToken}`
         }
     })
-
+    console.log(result, "RES")
     const collectionRefBonus = mongoose.connection.db.collection('ref-bonus')
     const collectionRefLinks = mongoose.connection.db.collection('ref-links')
 
@@ -21,18 +25,22 @@ router.get('/ref/info', async (req, res) => {
     const refLink = await collectionRefLinks.findOne({ uuid: dragonflyUUID })
     const refBonus = await collectionRefBonus.findOne({ refUUID: dragonflyUUID })
 
+    if (!refLink) return res.status(400).send({ success: false, message: `Your account is not connected with a ref link.` })
+
     if (!refBonus) return res.status(400).send({ success: false, message: `No bonus found for ${result.data.username}` })
     console.log(result.data, refBonus)
 
     res.status(200).send({ success: true, type: refLink.type, amount: refBonus.amount, creationDate: refBonus.creationDate })
 })
 
-router.post('/ref/type/:type', async (req, res) => {
-    const type = req.params.type
+router.post('/ref/type/', async (req, res) => {
+    const type = req.body.type
+    console.log(type, "TYPE")
+
     const dragonflyToken = req.cookies["dragonfly-token"]
     const validRefTypes = ["discount", "bonus"]
 
-    if (!validRefTypes.includes(type)) return res.status(400).send({ message: "Invalid ref type" })
+    if (!validRefTypes.includes(type)) return res.status(400).send({ success: false, message: "Invalid ref type" })
 
     if (!dragonflyToken) return res.send({ success: false, message: 'A dragonfly token is required' })
 
@@ -46,13 +54,13 @@ router.post('/ref/type/:type', async (req, res) => {
 
     const collectionRefLinks = mongoose.connection.db.collection('ref-links')
     const refLink = await collectionRefLinks.findOne({ uuid: dragonflyUUID })
-    if (refLink.type === type) return res.status(200).send({ message: `Ref type already set to ${type}` })
+    if (!refLink) return res.status(400).send({ success: false, message: `Referral with this name could not be found` })
+    if (refLink.type === type) return res.status(200).send({ success: false, message: `Ref type already set to ${type}` })
 
     const updatedRefLink = await collectionRefLinks.findOneAndUpdate({ uuid: dragonflyUUID }, { $set: { type: type } })
 
-    if (!refLink) return res.status(400).send({ message: `Referral with name ${dragonflyUUID} could not found` })
     console.log(updatedRefLink)
-    res.send({ uuid: dragonflyUUID, type: type, ref: updatedRefLink.value })
+    res.send({ success: true, uuid: dragonflyUUID, type: type, ref: updatedRefLink.value })
 })
 
 module.exports = router
